234c97a47a846c05a7bcba3afe33674d6366ff43
schen133 <schen133@u.rochester.edu>
2023-11-18 20:31:07 -0500

    produces txt files now

--- disassem.rb
+++ disassem.rb
@@ -1,13 +1,43 @@
 # Initialization
 require 'enumerator'
+
+# get C program name
 program_name = ARGV[0]
 if program_name == nil
     puts "No program"
     exit
 end
 
+# Output: objdump.txt
+# Command: objdump -d ARGV[0]
 objdump = "objdump -d #{program_name}"
+
+# Output: llvm-dwarfdump.txt
+# Command: llvm-dwarfdump --debug-line ARGV[0]
 dwarfdump = "llvm-dwarfdump --debug-line #{program_name}"
 obj = `#{objdump}`
 dwarf = `#{dwarfdump}`
-puts obj
+# puts "This is objDump result:"
+# puts obj
+# puts "This is dwarf result:"
+# puts dwarf
+
+
+# make files
+dwarfdump_file = "llvm-dwarfdump.txt"
+objdump_file = "objdump.txt"
+
+# write into file
+File.open(dwarfdump_file, 'w') do |file|
+  file.puts dwarf
+end
+
+File.open(objdump_file, 'w') do |file|
+  file.puts obj
+end
+
+
+
+
+
+
