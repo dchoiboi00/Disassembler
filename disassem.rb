5656dbfdc995feba6c34680749991a10a5a92ccb
schen133 <schen133@u.rochester.edu>
2023-11-19 17:04:34 -0500

    ruby code organized better

--- disassem.rb
+++ disassem.rb
@@ -22,7 +22,46 @@ dwarf = `#{dwarfdump}`
 # puts "This is dwarf result:"
 # puts dwarf
 
-# make files
+
+# Part 1: source code scraper
+# open source code file
+begin
+  file = File.open("#{program_name}.c", "r")
+rescue Errno::ENOENT
+  puts "Source code file not found"
+  exit
+end
+
+lineNum = 1
+finalSourceCode = ""
+file.each_line do |line|
+  # puts "#{lineNum}#{line}"
+  # remove new line from line
+  cleaned_line = line.chomp 
+
+  spacesBeforeNum =
+    if lineNum < 10
+      "&nbsp;&nbsp;"
+    elsif lineNum < 100
+      "&nbsp;"
+    else
+      ""
+    end
+
+  finalSourceCode << "<button>#{spacesBeforeNum}#{lineNum}</button> <span id=\"s1\" aline=\"\" style=\"background-color: white;\">#{cleaned_line}</span>\n"
+  
+  lineNum = lineNum + 1
+
+end
+file.close
+# puts finalSourceCode
+
+# source code scraper done 
+
+# TODO
+# Part 2: assembly code scraper 
+
+# make the txt files needed
 dwarfdump_file = "llvm-dwarfdump.txt"
 objdump_file = "objdump.txt"
 
@@ -35,6 +74,16 @@ File.open(objdump_file, 'w') do |file|
   file.puts obj
 end
 
+# use obj and dwarf to come up with html 
+
+finalAssemblyCode = ""
+
+
+
+# html_template.gsub!('<%= program_name %>', program_name)
+# html_template.gsub!('<%= obj_dump_result %>', File.read(objdump_file))
+# html_template.gsub!('<%= dwarfdump_result %>', File.read(dwarfdump_file))
+
 # create a corresponding html file
 html_file = "#{program_name}_disassem.html"
 
@@ -42,35 +91,12 @@ html_file = "#{program_name}_disassem.html"
 html_template = File.read("template.html")
 
 # putting stuff in template:
-# open source code file
-begin
-  file = File.open("#{program_name}.c", "r")
-rescue Errno::ENOENT
-  puts "Source code file not found"
-  exit
-end
-
-lineNum = 1
-finalSourceCode = ""
-file.each_line do |line|
-    # puts "#{lineNum}#{line}"
-  # remove new line from line
-  cleaned_line = line.chomp 
-  finalSourceCode << "<button>&nbsp;&nbsp;#{lineNum}</button> <span id=\"s1\" aline=\"\" style=\"background-color: white;\">#{cleaned_line}</span>\n"
-  lineNum = lineNum + 1
-end
-file.close
-
-
-# puts replace placeholder from template with what we just cameup with
+# replace placeholder from template with what we just came up with
 html_template.gsub!('{source_code_placeholder}', finalSourceCode)
-# puts finalSourceCode
+# html_template.gsub!('{assembly_code_placeholder}', finalAssemblyCode)
 
-# html_template.gsub!('<%= program_name %>', program_name)
-# html_template.gsub!('<%= obj_dump_result %>', File.read(objdump_file))
-# html_template.gsub!('<%= dwarfdump_result %>', File.read(dwarfdump_file))
+# open file we created and use the html template we just manipulated.
 
-# open file we created and write the template do it.
 File.open(html_file, 'w') do |file|
   file.puts html_template
 end
